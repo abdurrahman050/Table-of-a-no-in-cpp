6d28842b898514884c880fe7db3985e9aa902682
abdurrahman050 <abdurrahmanmunir5149@gmail.com>
2024-10-15 14:35:12 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
+void printNumber(int n);
 int main()
 {
-  cout<<"\nHello World :) ");
+  int n;
+  cout<<"\nEnter Number");
+  cin>>n;
+  printNumber(n);
   return 0;
+}
+void printNumber(int n)
+{
+  for(int i=0;i<10;i++)
+{
+  cout<<n<<" * "<<i+1<<" = "<<n*1;
+}
 }
